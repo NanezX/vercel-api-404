a267b63463fbb1a4e1629a0395fdbd28eb0d05bc
NanezX <victor.nanezj@gmail.com>
2024-04-17 17:03:35 -0400

    dto for token response

---
+++ src/dto/token-metadata-response.dto.ts
@@ -0,0 +1,31 @@
+import { IsString, ValidateNested, IsArray, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Attributes {
+  @ApiProperty({ example: 'eyes' })
+  @IsNotEmpty()
+  @IsString()
+  trait_type: string;
+
+  @ApiProperty({ example: 'green' })
+  @IsNotEmpty()
+  @IsString()
+  value: string;
+}
+
+export class TokenMetadataRespondeDto {
+  @ApiProperty({ example: 'www.token-obtain.com/get/2' })
+  @IsNotEmpty()
+  @IsString()
+  image: string;
+
+  @ApiProperty({ type: [Attributes] })
+  @IsNotEmpty()
+  @IsArray()
+  @ValidateNested({ each: true })
+  attributes: Attributes[];
+
+  constructor(data: { id: string }) {
+    this.image = data.id;
+  }
+}

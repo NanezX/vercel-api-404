3266dd78c9f3555c128edb17306de64fdc952784
NanezX <victor.nanezj@gmail.com>
2024-04-17 17:04:03 -0400

    update the config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import * as cookieParser from 'cookie-parser';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    credentials: true,
+    origin: true,
+  });
+
+  // app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('SM-SOC')
+    .setDescription('The SM-SOC API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  const globalPrefix = 'api';
+
+  app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const port = process.env.PORT || 3333;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
 }
+
 bootstrap();

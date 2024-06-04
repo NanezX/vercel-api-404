ce722fb5ca3887757dc3a4ed362074645f10f384
NanezX <victor.nanezj@gmail.com>
2024-06-04 18:11:10 -0400

    support for image

--- src/lib/utils/ipfs.ts
+++ src/lib/utils/ipfs.ts
@@ -1,3 +1,4 @@
+import * as async from 'async';
 import { Variants, getContract } from '$lib';
 
 type FetchIpfsOptions = {
@@ -66,4 +67,33 @@ export async function getVariants(): Promise<Variants> {
 
 	// Return the Variants class using the JSON created from the Buffer
 	return new Variants(JSON.parse(respBuffer.toString()), baseUrl);
+}
+
+export async function getImageBuffers(urls: URL[]): Promise<Buffer[]> {
+	// Max concurrency limit
+	const limit = 5;
+
+	const iteratorFn = async (url: URL) => {
+		const errorMsg = 'Failed to fetch layer';
+		try {
+			return await fetchIpfs(url, {
+				cache: false,
+				errorMsg
+			});
+		} catch (error) {
+			throw new Error(`${errorMsg}: \n${error}`);
+		}
+	};
+
+	return new Promise((resolve, reject) => {
+		async.mapLimit(urls, limit, iteratorFn, (err, results: (Buffer | undefined)[] | undefined) => {
+			if (err || results === undefined || results.some((result) => result === undefined)) {
+				return reject(err || new Error('Undefined result'));
+			}
+
+			// Now we can safely assume that all values in results are defined
+			const validResults: Buffer[] = results as Buffer[];
+			resolve(validResults);
+		});
+	});
 }
---
+++ src/lib/utils/picture.ts
@@ -0,0 +1,44 @@
+import { getDnaJson } from './contract';
+import { getImageBuffers, getVariants } from './ipfs';
+import sharp from 'sharp';
+
+export async function getPicture(id: string) {
+	// Get variants to make just one fetch
+	const variants = await getVariants();
+
+	// Obtain the DNA JSON string from contract based on ID
+	const dnaJson = await getDnaJson(id, {
+		variantCounters: variants.getCounters()
+	});
+
+	// Use the variant class to resolve all the layer URLs based on the dna json
+	const layersUrls = variants.resolveVariants(dnaJson);
+
+	// Get all the images buffer based on the layer URLs resolved
+	const images = await getImageBuffers(layersUrls);
+
+	// Create the composite layers with the info that we want
+	const compositeLayers = images.map((image) => ({
+		input: image,
+		top: 0,
+		left: 0
+	}));
+
+	// Get the Width and Height of the images
+	const { width, height } = await sharp(images[0]).metadata();
+
+	// Composite the images with this value to guarantee the transparency
+	const result = await sharp({
+		create: {
+			width,
+			height,
+			channels: 4,
+			background: { r: 0, g: 0, b: 0, alpha: 0 }
+		}
+	})
+		.composite(compositeLayers)
+		.png()
+		.toBuffer();
+
+	return result;
+}

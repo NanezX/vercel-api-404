d16750c26fb26164b5208912c5a5a33a3aacbb49
NanezX <victor.nanezj@gmail.com>
2024-06-04 18:11:29 -0400

    missing api prefix

--- src/routes/api/token/[id]/dto/token-metadata-response.dto.ts
+++ src/routes/api/token/[id]/dto/token-metadata-response.dto.ts
@@ -7,7 +7,7 @@ export class TokenMetadataRespondeDto {
 	description: string;
 
 	constructor(id_: string, dnaJson: string, name_: string, urlBase_: string) {
-		this.image = `${urlBase_}/picture/${id_}`;
+		this.image = `${urlBase_}/api/picture/${id_}`;
 		this.name = name_;
 		this.description = 'The best collection using ERC404 is Pet404';
 		this.attributes = Attribute.fromJson(dnaJson);
